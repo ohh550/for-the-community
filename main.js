1131dbf1e70cb8f0f78cab18eb237a7fa53cb9dd
ohh550 <102753073+ohh550@users.noreply.github.com>
2022-04-28 13:44:34 -0400

    my first commmit

---
+++ main.js
@@ -0,0 +1,17 @@
+let img; //remember we need to create a variable for our image
+  //then we need to load the image in preload before setup
+  function preload(){
+   img = loadImage("Ducky.jpg");
+  }
+  
+  function setup(){
+    let c = createCanvas(400,400);
+    c.parent('canvas-wrapper');
+  }
+  
+  function draw(){
+    ellipse(200,200,50,50);
+    //then we will draw the image where the mouse is
+    image(img, mouseX, mouseY, 100, 100);
+  }
+  
